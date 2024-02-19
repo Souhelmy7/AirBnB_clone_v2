9be137766b7a3338a53bb26001d56d7af3fa92d2
souhelmy7 <esraahelmy77@gmail.com>
2024-02-19 18:42:42 +0000

    ok commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-'''8. List of states'''
+"""
+this module starts a Flask web application
+"""
 
-from flask import Flask, render_template
-from models import storage
-from models.state import State
+from flask import Flask
+
 
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def show_states(id=None):
-    """show states"""
-    states = storage.all(State)
-    try:
-        state = states['State.' + id] if id is not None else None
-    except (KeyError):
-        return render_template('9-states.html', states=None, state=None)
-    return render_template('9-states.html',
-                           states=states.values(), state=state)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """function to print hello HBNB when following the url /"""
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
